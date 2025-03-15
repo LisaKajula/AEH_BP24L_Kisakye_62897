8d04c8184e30994ca8d1c742765c43b59b719c94
lisakajula@gail.com <lisakajula@gmail.com>
2025-03-15 07:53:26 +0100

    hello world , lab 1 \

---
+++ src/pl/pp/myFirstApp.java
@@ -0,0 +1,7 @@
+package pl.pp;
+
+public class myFirstApp {  // Class names should follow PascalCase convention
+    public static void main(String[] args) {
+        System.out.println("Hello Basics Of Programming!");
+    }
+}
